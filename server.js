80f7b495b44b8388ee89c24c6996e1385d75ac49
Baroka <baroka@BarokanoMacBook-Pro.local>
2024-08-02 17:57:54 +0100

    update port

--- server.js
+++ server.js
@@ -21,6 +21,6 @@ app.use('/api/coaches', coachRoutes);
 app.use('/api/history', historyRoutes);
 app.use('/api/appointments', appointmentRoutes);
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5050;
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
